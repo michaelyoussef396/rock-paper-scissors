1a0d17e93ab78a7c7690518f8418d611d6ebcd41
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-03-18 14:50:24 +1100

    Finalize game loop with score display and closing message for rock-paper-scissors game

--- main.py
+++ main.py
@@ -18,3 +18,24 @@ while True:
 
     computer_pick = options[random_number]
     print("Computer picked", computer_pick + ".")
+
+    if user_input == 'rock' and computer_pick == 'scissors':
+        print("You won!")
+        user_wins += 1
+        continue
+    elif user_input == 'paper' and computer_pick == 'rock':
+        print("You won!")
+        user_wins += 1
+        continue
+    elif user_input == 'scissors' and computer_pick == 'paper':
+        print("You won!")
+        user_wins += 1
+        continue
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+
+print("thanks for playing")
